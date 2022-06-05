2113d605161028ad429d4cce8b958c7f7f75ccb1
gilden12 <87315003+gilden12@users.noreply.github.com>
2022-06-05 21:44:13 +0300

    Normaly

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PriceWindowController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PriceWindowController.java
@@ -3,41 +3,39 @@ package il.cshaifasweng.OCSFMediatorExample.client;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import il.cshaifasweng.OCSFMediatorExample.entities.CatalogData;
-import il.cshaifasweng.OCSFMediatorExample.client.App;
-import il.cshaifasweng.OCSFMediatorExample.client.SimpleClient;
 
 import java.io.IOException;
 
 public class PriceWindowController {
 
     @FXML
-    private Label nameLabel;
+    private TextField PriceTF;
 
     @FXML
-    private Button priceButton;
+    private TextField descritionTF;
 
     @FXML
-    private Button showCatalog;
+    private TextField nameTF;
 
     @FXML
-    private TextField textField;
+    private Button showCatalogButton;
 
     @FXML
-    void changePriceAction(ActionEvent event) {
-        SimpleClient myclient=SimpleClient.getClient();
-        myclient.changePrice(Integer.parseInt(textField.getText()),App.data.itemsdata.get(App.thisitem));
-    }
+    private Button updateButton;
 
     @FXML
     void showCatalolgWindow(ActionEvent event) throws IOException {
-        try {
-            App.setRoot("PrimaryCatalog");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        App.setRoot("PrimaryCatalog");
+    }
+
+    @FXML
+    void updateValues(ActionEvent event) {
+        SimpleClient myclient=SimpleClient.getClient();
+        myclient.changePrice(Integer.parseInt(nameTF.getText()),App.data.itemsdata.get(App.thisitem));
+
+
     }
 
 }
+
